6460e342396e7c249d605ac5e9b4bf1964b40fc8
janko <janko@DESKTOP-8USDVD4>
2023-10-01 15:05:27 +0200

    Projektdateien hinzufügen.

---
+++ MasterApplication/MasterApplication.cpp
@@ -0,0 +1,12 @@
+﻿// MasterApplication.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "MasterApplication.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ MasterApplication/MasterApplication.h
@@ -0,0 +1,8 @@
+﻿// MasterApplication.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
