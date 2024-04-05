77dfbb7d27c65887abf91da6fb278e5f22c8bff9
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-04-05 12:28:08 +0200

    added TODOs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and parsed");
   });
+
+//TODO: Jquery to Javascript converter
+//TODO: calculate distance between coordinates (great circle, ...)
