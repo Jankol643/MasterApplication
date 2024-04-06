1f6fd2d126548acbcea8bd2de68d0e49a9c4eff1
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-04-06 10:56:17 +0200

    error correction

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,10 +1,7 @@
-const Singleton = (function () {
-  let instance;
-
   function test() {
     //TODO: read from JSON file
-    //var fs  = require('fs');
-    var obj = {
+    //let fs  = require('fs');
+    let obj = {
       "europe": {
         "poland": {},
         "france": {},
@@ -21,35 +18,19 @@ const Singleton = (function () {
   }
 
   function createStructure(folder, o) {
-      for (let key in o) {
-        if (typeof o[key] === 'object' && o[key] !== null) {
-          console.log('folder created : ' + (folder + key))
-          //fs.mkdir(folder + key, function() {
-          if (Object.keys(o[key]).length) {
-            create(folder + key + '/', o[key]);
-          }
-          //});
-        } else {
-          console.log('file created : ' + (folder + key + (o[key] === null ? '' : '.' + o[key])))
-          //fs.writeFile(folder + key + (o[key] === null ? '' : '.' + o[key]));
+    for (let key in o) {
+      if (typeof o[key] === 'object' && o[key] !== null) {
+        console.log('folder created : ' + (folder + key))
+        //fs.mkdir(folder + key, function() {
+        if (Object.keys(o[key]).length) {
+          create(folder + key + '/', o[key]);
         }
+        //});
+      } else {
+        console.log('file created : ' + (folder + key + (o[key] === null ? '' : '.' + o[key])))
+        //fs.writeFile(folder + key + (o[key] === null ? '' : '.' + o[key]));
       }
+    }
   };
 
-  function createInstance() {
-      const object = new Object({ name: "Singleton Object" });
-      return object;
-  }
-
-  return {
-      getInstance: function () {
-          if (!instance) {
-              instance = createInstance();
-          }
-          return instance;
-      },
-  };
-});
-
-const singleton1 = Singleton.getInstance();
-export default singleton1;
+export {test, createStructure};
